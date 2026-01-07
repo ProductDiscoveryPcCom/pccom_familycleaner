26df2f6571fe08a54087aca2978710d8a6f8d777
ProductDiscoveryPcCom <product.discovery@pccomponentes.com>
2026-01-07 16:31:56 +0100

    Add files via upload

---
+++ facet-analyzer/utils/__init__.py
@@ -0,0 +1,16 @@
+from .data_processor import DataProcessor, AnalysisResults
+from .analyzers import FacetAnalyzer, IndexationAnalyzer, InsightGenerator, ArchitectureAnalyzer, NavigationSystemGenerator
+from .llm_validator import LLMValidator
+from .report_generator import ReportGenerator
+
+__all__ = [
+    'DataProcessor',
+    'AnalysisResults', 
+    'FacetAnalyzer',
+    'IndexationAnalyzer',
+    'InsightGenerator',
+    'ArchitectureAnalyzer',
+    'NavigationSystemGenerator',
+    'LLMValidator',
+    'ReportGenerator'
+]
